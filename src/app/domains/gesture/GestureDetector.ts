cf76332c5c26d9cd0ad97d7ef4d3220f0d7a6884
Yago de Castro <yagofernandodecastro@gmail.com>
2026-07-03 15:31:49 -0300

    fix: initialize FaceDetector with runningMode: 'VIDEO' to enable real-time video tracking

--- src/app/domains/gesture/GestureDetector.ts
+++ src/app/domains/gesture/GestureDetector.ts
@@ -93,10 +93,13 @@ export class GestureDetector implements IGestureDetector {
       vision,
       this.mediaPipeConfig.handLandmarkerOptions
     )
-    this.faceDetector = await FaceDetector.createFromModelPath(
-      vision,
-      'https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite'
-    )
+    this.faceDetector = await FaceDetector.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite',
+        delegate: 'GPU'
+      },
+      runningMode: 'VIDEO'
+    })
     await new Promise(resolve => setTimeout(resolve, this.mediaPipeConfig.initTimeout))
     this.isMediaPipeReady = true
   }
